aca140a01dd3f37bd880f2323d776a11bb01c4b9
Peter Wagenet <peter.wagenet@gmail.com>
2009-11-26 00:24:01 -0500

    Support for basic styling, aligning, and coloration

--- views/editor.js
+++ views/editor.js
@@ -22,6 +22,7 @@ RichText.EditorView = SC.FieldView.extend(
   editorIsReady: NO,
 
   selection: '',
+  selectionElement: null,
   cursorPos: null,
 
   toolbarView: RichText.ToolbarView,
@@ -367,23 +368,43 @@ RichText.EditorView = SC.FieldView.extend(
 
 // Utility methods
 
-  // Borrowed from github.com/etgryphon/sproutcore-ui
+  // Based on github.com/etgryphon/sproutcore-ui
   querySelection: function() {
-    var selection = '';
-
     if (SC.browser.msie) {
-      selection = this._iframe.document.selection.createRange().text;
+      var rawSelection = this._iframe.document.selection.createRange(),
+          selection = rawSelection.text(),
+          selectionElement;
+
       if (SC.none(selection)) selection = '';
 
+      selectionElement = rawSelection.parentElement();
     } else {
-      var inputWindow = this.$inputWindow().get(0);
-      selection = inputWindow.getSelection();
+      var selection = this.$inputWindow().get(0).getSelection(),
+          selectionRange = selection.getRangeAt(0),
+          commonAncestor = selectionRange.commonAncestorContainer,
+          childNodes = commonAncestor.childNodes,
+          selectionNodes = [], childNode, idx;
+
+      for(idx=0; idx < childNodes.length; idx++) {
+        childNode = childNodes[idx];
+        // Check for full or partial containment and make sure it's not an empty text node
+        if (selection.containsNode(childNode, true) && !(childNode.nodeType === 3 && childNode.length === 0)) {
+          selectionNodes.push(childNode);
+        }
+      }
+
+      // If only one selection node and it's an element, use that
+      if (selectionNodes.length === 1 && selectionNodes[0].nodeType === 1) {
+        selectionElement = selectionNodes[0];
+      } else {
+        selectionElement = commonAncestor;
+      }
+
+      while(selectionElement && selectionElement.nodeType !== 1) selectionElement = selectionElement.parentNode;
     }
 
-    // TODO: Are these notifiers necessary?
-    this.propertyWillChange('selection');
-    this.set('selection', selection.toString());
-    this.propertyDidChange('selection');
+    this.setIfChanged('selection', selection.toString());
+    this.setIfChanged('selectionElement', selectionElement);
   },
 
   // Based on http://niichavo.wordpress.com/2009/01/06/contentEditable-div-cursor-position/
@@ -462,13 +483,329 @@ RichText.EditorView = SC.FieldView.extend(
 
 // Editor actions
 
-  boldSelection: function() {
-    this.iframeExecCommand('bold', false, null);
+  // From wysihat
+  _standardizeColor: function(color) {
+    var idx;
+
+    if (!color || color.match(/[0-9a-f]{6}/i)) return color;
+    var m = color.toLowerCase().match(/^(rgba?|hsla?)\(([\s\.\-,%0-9]+)\)/);
+    if(m){
+      var c = m[2].split(/\s*,\s*/), l = c.length, t = m[1];
+      if((t == "rgb" && l == 3) || (t == "rgba" && l == 4)){
+        var r = c[0];
+        if(r.charAt(r.length - 1) == "%"){
+          var a = [];
+          for(idx=0; idx < c.length; idx++) a.push(parseFloat(c[idx]) * 2.56);
+          if(l == 4){ a[3] = c[3]; }
+          return this._colorFromArray(a);
+        }
+        return this._colorFromArray(c);
+      }
+      if((t == "hsl" && l == 3) || (t == "hsla" && l == 4)){
+        var H = ((parseFloat(c[0]) % 360) + 360) % 360 / 360,
+          S = parseFloat(c[1]) / 100,
+          L = parseFloat(c[2]) / 100,
+          m2 = L <= 0.5 ? L * (S + 1) : L + S - L * S,
+          m1 = 2 * L - m2,
+          a = [this._hue2rgb(m1, m2, H + 1 / 3) * 256,
+            this._hue2rgb(m1, m2, H) * 256, this._hue2rgb(m1, m2, H - 1 / 3) * 256, 1];
+        if(l == 4){ a[3] = c[3]; }
+        return this._colorFromArray(a);
+      }
+    }
+    return null; // dojo.Color
   },
+
+  // From wysihat
+  _colorFromArray: function(a) {
+    var arr = [], idx;
+    for(idx=0; idx < 3 && idx < a.length; idx++){
+      var s = parseInt(a[idx], 10).toString(16);
+      arr.push(s.length < 2 ? "0" + s : s);
+    };
+    return "#" + arr.join(""); // String
+  },
+
+  _hue2rgb: function(m1, m2, h){
+    if(h < 0){ ++h; }
+    if(h > 1){ --h; }
+    var h6 = 6 * h;
+    if(h6 < 1){ return m1 + (m2 - m1) * h6; }
+    if(2 * h < 1){ return m2; }
+    if(3 * h < 2){ return m1 + (m2 - m1) * (2 / 3 - h) * 6; }
+    return m1;
+  },
+
+  // borrowed with slight changes from jQuery
+  getStyle: function(elem, name, force) {
+    var defaultView = this.$inputWindow().get(0),
+        ret, style = elem.style;
+
+    // A helper method for determining if an element's values are broken
+    function color( elem ) {
+      if ( !SC.browser.safari )
+        return false;
+
+      var ret = defaultView.getComputedStyle( elem, null );
+      return !ret || ret.getPropertyValue("color") == "";
+    }
+
+    // We need to handle opacity special in IE
+    if ( name == "opacity" && SC.browser.msie ) {
+      ret = SC.$.attr( style, "opacity" );
+
+      return ret == "" ?
+        "1" :
+        ret;
+    }
+    // Opera sometimes will give the wrong display answer, this fixes it, see #2037
+    if ( SC.browser.opera && name == "display" ) {
+      var save = style.outline;
+      style.outline = "0 solid black";
+      style.outline = save;
+    }
+
+    // Make sure we're using the right name for getting the float value
+    if ( name.match( /float/i ) )
+      name = styleFloat;
+
+    if ( !force && style && style[ name ] )
+      ret = style[ name ];
+
+    else if ( defaultView.getComputedStyle ) {
+
+      // Only "float" is needed here
+      if ( name.match( /float/i ) )
+        name = "float";
+
+      name = name.replace( /([A-Z])/g, "-$1" ).toLowerCase();
+
+      var computedStyle = defaultView.getComputedStyle( elem, null );
+
+      if ( computedStyle && !color( elem ) )
+        ret = computedStyle.getPropertyValue( name );
+
+      // If the element isn't reporting its values properly in Safari
+      // then some display: none elements are involved
+      else {
+        var swap = [], stack = [], a = elem, i = 0;
+
+        // Locate all of the parent display: none elements
+        for ( ; a && color(a); a = a.parentNode )
+          stack.unshift(a);
+
+        // Go through and make them visible, but in reverse
+        // (It would be better if we knew the exact display type that they had)
+        for ( ; i < stack.length; i++ )
+          if ( color( stack[ i ] ) ) {
+            swap[ i ] = stack[ i ].style.display;
+            stack[ i ].style.display = "block";
+          }
+
+        // Since we flip the display style, we have to handle that
+        // one special, otherwise get the value
+        ret = name == "display" && swap[ stack.length - 1 ] != null ?
+          "none" :
+          ( computedStyle && computedStyle.getPropertyValue( name ) ) || "";
+
+        // Finally, revert the display styles back
+        for ( i = 0; i < swap.length; i++ )
+          if ( swap[ i ] != null )
+            stack[ i ].style.display = swap[ i ];
+      }
+
+      // We should always get a number back from opacity
+      if ( name == "opacity" && ret == "" )
+        ret = "1";
+
+    } else if ( elem.currentStyle ) {
+      var camelCase = name.replace(/\-(\w)/g, function(all, letter){
+        return letter.toUpperCase();
+      });
+
+      ret = elem.currentStyle[ name ] || elem.currentStyle[ camelCase ];
+
+      // From the awesome hack by Dean Edwards
+      // http://erik.eae.net/archives/2007/07/27/18.54.15/#comment-102291
+
+      // If we're not dealing with a regular pixel number
+      // but a number that has a weird ending, we need to convert it to pixels
+      if ( !/^\d+(px)?$/i.test( ret ) && /^\d/.test( ret ) ) {
+        // Remember the original values
+        var left = style.left, rsLeft = elem.runtimeStyle.left;
+
+        // Put in the new values to get a computed value out
+        elem.runtimeStyle.left = elem.currentStyle.left;
+        style.left = ret || 0;
+        ret = style.pixelLeft + "px";
+
+        // Revert the changed values
+        style.left = left;
+        elem.runtimeStyle.left = rsLeft;
+      }
+    }
+
+    return ret;
+  },
+
+  getSelectionStyle: function(name, force) {
+    return this.getStyle(this.get('selectionElement'), name, force);
+  },
+
+  defaultColor: function() {
+    var body = this.$inputBody().get(0);
+    return this._standardizeColor(this.getStyle(body, 'color'));
+  }.property().cacheable(),
+
+  defaultBackgroundColor: function(){
+    var body = this.$inputBody().get(0);
+    return this._standardizeColor(this.getStyle(body, 'background-color'));
+  }.property().cacheable(),
+
+  _basicSelectionModifier: function(property, type, val, related) {
+    if (!this.get('editorIsReady')) return false;
+
+    if (val !== undefined) {
+      this.propertyWillChange(property);
+      var x = this.iframeExecCommand(type, false, val);
+      this.propertyDidChange(property);
+
+      if(x) {
+        this._field_fieldValueDidChange();
+
+        if (related) {
+          for (var idx=0; idx < related.length; idx++){
+            this.notifyPropertyChange(related[idx]);
+          }
+        }
+      }
+    }
+
+    return this.iframeQueryState(type);
+  },
+
+  selectionColor: function(key, val) {
+    if (!this.get('editorIsReady')) return null;
+
+    if (val !== undefined) {
+      this.propertyWillChange('selectionColor');
+      var x = this.iframeExecCommand('forecolor', false, val);
+      this.propertyDidChange('selectionColor');
+
+      if(x) this._field_fieldValueDidChange();
+    }
+
+    return this._standardizeColor(this.getSelectionStyle('color'));
+
+  }.property('selectionElement').cacheable(),
+
+  selectionBackgroundColor: function(key, val) {
+    if (!this.get('editorIsReady')) return null;
+
+    var bgCommand = SC.browser.mozilla ? 'hilitecolor' : 'backcolor';
+
+    if (val !== undefined) {
+      this.propertyWillChange('selectionBackgroundColor');
+      var x = this.iframeExecCommand(bgCommand, false, val);
+      this.propertyDidChange('selectionBackgroundColor');
+
+      if(x) this._field_fieldValueDidChange();
+    }
+
+    return this._standardizeColor(this.getSelectionStyle('background-color'));
+
+  }.property('selectionElement').cacheable(),
+
+  selectionIsBold: function(key, val) {
+    return this._basicSelectionModifier('selectionIsBold', 'bold', val);
+  }.property('selection').cacheable(),
+
+  selectionIsUnderlined: function(key, val) {
+    return this._basicSelectionModifier('selectionIsUnderlined', 'underline', val);
+  }.property('selection').cacheable(),
+
+  selectionIsItalicized: function(key, val) {
+    return this._basicSelectionModifier('selectionIsItalicized', 'italic', val);
+  }.property('selection').cacheable(),
+
+  selectionIsStrikethrough: function(key, val) {
+    return this._basicSelectionModifier('selectionIsStrikethrough', 'strikethrough', val);
+  }.property('selection').cacheable(),
+
+  selectionIsLeftAligned: function(key, val) {
+    return this._basicSelectionModifier('selectionIsLeftAligned', 'justifyleft', val,
+                                          'selectionIsJustified selectionIsCentered selectionIsRightAligned'.w());
+  }.property('selection').cacheable(),
+
+  selectionIsJustified: function(key, val) {
+    return this._basicSelectionModifier('selectionIsJustified', 'justifyfull', val,
+                                          'selectionIsLeftAligned selectionIsCentered selectionIsRightAligned'.w());
+  }.property('selection').cacheable(),
+
+  selectionIsCentered: function(key, val) {
+    return this._basicSelectionModifier('selectionIsCentered', 'justifycenter', val,
+                                          'selectionIsLeftAligned selectionIsJustified selectionIsRightAligned'.w());
+  }.property('selection').cacheable(),
+
+  selectionIsRightAligned: function(key, val) {
+    return this._basicSelectionModifier('selectionIsRightAligned', 'justifyright', val,
+                                        'selectionIsLeftAligned selectionIsJustified selectionIsCentered'.w());
+  }.property('selection').cacheable(),
+
+  selectionIsDefaultColor: function(key, val) {
+    var selectionColor, selectionBackgroundColor;
+
+    if (val === YES) {
+      this.set('selectionColor', 'inherit');
+      this.set('selectionBackgroundColor', 'inherit');
+    }
+
+    selectionColor = this.get('selectionColor');
+    selectionBackgroundColor = this.get('selectionBackgroundColor');
+
+    return (CmndoSprout.blank(selectionColor) || selectionColor === this.get('defaultColor'))
+            && (CmndoSprout.blank(selectionBackgroundColor) || selectionBackgroundColor === this.get('defaultBackgroundColor'));
+
+  }.property('selectionColor', 'selectionBackgroundColor').cacheable(),
+
+  selectionIsHighlighted: function(key, val) {
+    var selectionColor;
+
+    if (val !== undefined) {
+      val = (val) ? '#ffff00' : null; // yellow
+      this.set('selectionColor', val);
+    }
+
+    selectionColor = this.get('selectionColor');
+
+    return !CmndoSprout.blank(selectionColor)
+            && selectionColor !== this.get('defaultColor');
+
+  }.property('selectionColor').cacheable(),
+
+  selectionIsBackgroundHighlighted: function(key, val) {
+    var selectionBackgroundColor;
+
+    if (val !== undefined) {
+      val = (val) ? '#ffff00' : null; // yellow
+      this.set('selectionBackgroundColor', val);
+    }
+
+    selectionBackgroundColor = this.get('selectionBackgroundColor');
+
+    return !CmndoSprout.blank(selectionBackgroundColor)
+            && selectionBackgroundColor !== this.get('defaultBackgroundColor');
+
+  }.property('selectionBackgroundColor').cacheable(),
 
   iframeExecCommand: function() {
     var inputDocumentInstance = this.$inputDocument().get(0);
     return inputDocumentInstance.execCommand.apply(inputDocumentInstance, arguments);
+  },
+
+  iframeQueryState: function() {
+    var inputDocumentInstance = this.$inputDocument().get(0);
+    return inputDocumentInstance.queryCommandState.apply(inputDocumentInstance, arguments);
   }
 
 });
