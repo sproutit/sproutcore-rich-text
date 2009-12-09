da0c47fe6ccf427f9a2881a43aa4f8c503bd05fe
Peter Wagenet <peter.wagenet@gmail.com>
2009-12-08 20:16:21 -0500

    Version bump

--- core.js
+++ core.js
@@ -14,7 +14,7 @@ RichText = SC.Object.create(
   /** @scope RichText.prototype */ {
 
   NAMESPACE: 'RichText',
-  VERSION: '0.3.1',
+  VERSION: '0.3.2',
 
   // TODO: Add global constants or singleton objects needed by your app here.
 
